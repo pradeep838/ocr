17fe3e2446934a1139bddd58c2db77db600a4ae2
Pradeep Kumar <kumarp@adobe.com>
2022-10-04 13:15:57 +0530

    added merging logice for cluster of word

--- v2/pixel.py
+++ v2/pixel.py
@@ -1,5 +1,9 @@
 
 
+from operator import contains
+from typing import Container
+
+
 def writelog(text):
     with open('./logs/temp.txt','a') as f:
         f.write(text)
@@ -12,25 +16,29 @@ def writelog(text):
 
 #Global Variable.
 MAX_ROWS=1081
-MAX_COLS=1921
+MAX_COLS=19201
 
 
 
-
-def getLocationMatrix(pixellocationOfword):
+# return a pixel matrix having row=Max_Rows and cols=Max_cols 
+# and each pixel[i][j] will map to ocr_extracted_text at location j,i 
+# and it pixel[i][j]=(ocr_extracted_text,x_cordinate,y_cordinate)
+def getLocationMatrix(ocr_extracted_dict):
     # print('running pixel matrix')
     pxiel_matrix = [[("",0,0) for i in range(MAX_COLS)] for j in range(MAX_ROWS)]
-    for key,value in pixellocationOfword.items():
-        for location in value:
-            pxiel_matrix[location[1]][location[0]]=(key,location[2],location[3])
+    for ocr_extracted_single_wrd,locations in ocr_extracted_dict.items():
+        for location in locations:
+            pxiel_matrix[location[1]][location[0]]=(ocr_extracted_single_wrd,location[2],location[3])
     return pxiel_matrix
             
 
 
-
+# logic is not correct
 
 
 def getClusterOfWordsWithInRectangle(single_word_set_of_all_possible_cordinate,pxiel_matrix):
+    # print("Debug:getClusterOfWordsWithInRectangle",single_word_set_of_all_possible_cordinate)
+    all_posiblity_contianer=[]
     for i in (single_word_set_of_all_possible_cordinate):
         contianer=[]
         row=i[1]
@@ -38,12 +46,16 @@ def getClusterOfWordsWithInRectangle(single_word_set_of_all_possible_cordinate,p
         # print(row,col)
         # for each vertical row search horizonally....
         vertical_margin_to_search=10
-        horizontal_margin_to_search=72
+        horizontal_margin_to_search=150
         for search_row in range(max(row-vertical_margin_to_search,0),min(row+vertical_margin_to_search,MAX_ROWS)):
             for search_col in range(col,min(col+horizontal_margin_to_search,MAX_COLS)):
                 if(not(pxiel_matrix[search_row][search_col][0]=='')):
                     contianer.append((pxiel_matrix[search_row][search_col][0],search_col,search_row,pxiel_matrix[search_row][search_col][1],pxiel_matrix[search_row][search_col][2]))
-        return contianer   #[(single word,x,y,w,h)]
+        horizontally_sorted=sorted(contianer,key=lambda x:x[1])
+        all_posiblity_contianer.append(horizontally_sorted)
+    # print("Debug:getClusterOfWordsWithInRectangle\n",all_posiblity_contianer)
+    # print("Debug:getClusterOfWordsWithInRectangle\n","-"*10)
+    return all_posiblity_contianer   #[[(single word,x,y,w,h)],[],[]]
 
 # pixel_matrix=getLocationMatrix(pixellocationOfword)
 
@@ -64,12 +76,15 @@ def getClusterOfWordsWithInRectangle(single_word_set_of_all_possible_cordinate,p
 def getClusterOfMultipleWords(str_of_text,ocr_extracted_dict,pixel_matrix):
     str_of_text=str_of_text.split(" ")
     container=[]
-    for single_word in str_of_text:
+    for single_word in str_of_text[0:1]:
+        # print("Debug:getClusterOfMultipleWords\n",single_word)
         if not ocr_extracted_dict.get(single_word)==None:
             single_word_cluster=getClusterOfWordsWithInRectangle(ocr_extracted_dict[single_word],pixel_matrix)
+            # print("Debug:getClusterOfMultipleWords\n",single_word_cluster)
             container.extend((single_word_cluster))
     
-    return sorted(container,key=lambda x:x[1]) #horizonatal sorting 
+    # return sorted(container,key=lambda x:x[1]) #horizonatal sorting 
+    return  container #horizonatal sorting 
 
 
 
@@ -130,17 +145,17 @@ def getLocation(string_of_text,ocr_extracted_dict):
                 return
         else:
             container=getClusterOfMultipleWords(string_of_text,ocr_extracted_dict,pixel_matrix)
-            vertical_aligned_text_container=get_vertically_aligned_text(container)
-            unique_vertical_aligned_text_container=get_unique(vertical_aligned_text_container)
-            container=unique_vertical_aligned_text_container
+            # vertical_aligned_text_container=get_vertically_aligned_text(container)
+            # unique_vertical_aligned_text_container=get_unique(vertical_aligned_text_container)
+            # container=unique_vertical_aligned_text_container
         writelog(string_of_text+"\n")
         writelog(str(container))
         writelog("- "*100)
-        if len(container)==1:
-                w=container[0][3]
-                h=container[0][4]
-                x=container[0][1]
-                y=container[0][2]
+        if len(container[0])==1:
+                w=container[0][0][3]
+                h=container[0][0][4]
+                x=container[0][0][1]
+                y=container[0][0][2]
                 return  (string_of_text,x,y,w,h,{'cluster_of_word':container})
         else:
                 w=container[-1][1]-container[0][1]
@@ -149,24 +164,29 @@ def getLocation(string_of_text,ocr_extracted_dict):
                 y=container[0][2]
                 return (string_of_text,x,y,w,h,{'cluster_of_word':container})
     except Exception as e:
-        print(e)
+        print("Debug:getLocationMatrix:",e)
    
 
-    print("Debug getLocationMatrix",container)
+    print("Debug:getLocationMatrix",container)
 
-    
-# ocr_extracted_dict={'Import': {(34, 76, 89, 23), (1113, 928, 73, 23)}, 'Events': {(2412, 884, 66, 16), (1462, 78, 84, 18)}, 'Search': {(1622, 75, 81, 20)}, 'Create': {(2581, 76, 81, 19)}, 'Share': {(2746, 75, 72, 20)}, 'Media': {(1038, 72, 74, 36), (1195, 927, 64, 20)}, 'Places': {(1322, 76, 79, 20)}, 'Albums': {(71, 134, 58, 29)}, 'i': {(211, 132, 30, 26)}, 'Folders': {(63, 207, 62, 15), (250, 138, 56, 15)}, 'Ly': {(1436, 131, 35, 28)}, 'Named': {(1487, 137, 67, 17)}, '2?': {(1620, 131, 37, 29)}, 'UnNamed': {(1671, 136, 95, 19)}, 'My': {(33, 208, 22, 18)}, 'M': {(375, 193, 20, 20)}, 'Hide': {(402, 197, 33, 15)}, 'Small': {(444, 197, 39, 15)}, 'Stacks': {(490, 197, 51, 15)}, 'All': {(2436, 249, 20, 18), (1573, 197, 20, 15)}, 'People': {(33, 1489, 40, 13), (2464, 249, 62, 23), (1602, 197, 54, 19)}, 'Ratings': {(2623, 198, 57, 17)}, 'e': {(2717, 199, 39, 10)}, 'ee': {(2771, 199, 39, 10)}, 'GS': {(59, 244, 23, 21)}, 'dataseti': {(99, 248, 68, 15)}, 'Groups': {(2812, 1459, 50, 15), (2437, 313, 67, 21)}, 'fm': {(2438, 365, 24, 15)}, 'Colleagues': {(2478, 362, 84, 18)}, 'fim': {(2438, 413, 24, 15)}, 'Family': {(2478, 410, 46, 17)}, 'fmm': {(2438, 461, 24, 15)}, 'Friends': {(2478, 458, 55, 15)}, 'Ungrouped': {(2437, 504, 104, 23)}, 'I': {(1941, 594, 34, 33)}, 'pixelpy': {(2079, 599, 104, 26)}, 'pythonG': {(2195, 599, 118, 27)}, 'F': {(2328, 595, 32, 31)}, 'GJ': {(2378, 595, 32, 31)}, '08': {(2499, 597, 30, 27)}, 'EXPLORER': {(2046, 669, 92, 15)}, 'pixels': {(2388, 668, 63, 23)}, 'D': {(2470, 666, 18, 23)}, 'O': {(2801, 1183, 22, 25), (2703, 812, 24, 24), (2540, 663, 76, 28)}, 'Vv': {(2038, 801, 17, 28), (2017, 727, 17, 9)}, 'PYTHONGUI': {(2046, 723, 119, 15)}, 'et': {(2615, 725, 18, 13)}, 'n': {(2708, 728, 16, 11)}, 'p': {(2133, 851, 10, 17), (2420, 721, 44, 24)}, 'rs': {(2509, 728, 11, 17)}, 'pytestcache': {(2047, 762, 165, 24), (2087, 969, 140, 24)}, 'daab': {(2483, 773, 101, 18)}, 'tT': {(2608, 770, 19, 20)}, 'J': {(2655, 770, 19, 20)}, 'x': {(2747, 774, 16, 15)}, 'Pa': {(2787, 785, 54, 6)}, 'ke': {(2073, 805, 21, 17)}, 'cr': {(2137, 811, 20, 10)}, 'te': {(1205, 751, 400, 95)}, '?': {(2061, 1059, 10, 10), (1367, 747, 53, 125)}, 'QD': {(1482, 751, 123, 95)}, 'Boyton': {(2505, 812, 101, 26)}, 'ty': {(2628, 812, 49, 24)}, 'Wi': {(2751, 812, 21, 24)}, 't': {(2054, 844, 38, 18)}, 'test': {(2072, 890, 37, 14)}, 'PS': {(2366, 881, 24, 16)}, 'Aa': {(2637, 881, 22, 14)}, 'ab': {(2679, 879, 21, 15)}, 'py': {(2366, 914, 25, 19)}, 'awe': {(2475, 923, 50, 7)}, 'to': {(468, 1460, 14, 12), (1268, 929, 21, 18)}, 'see': {(1298, 933, 38, 14)}, 'similar': {(1345, 927, 72, 20)}, 'looking': {(1426, 927, 76, 24)}, 'faces': {(1511, 927, 57, 20)}, 'grouped': {(1577, 927, 88, 24)}, 'pycache': {(2087, 927, 126, 24)}, 'automatically': {(1316, 959, 148, 23)}, 'testpy': {(2378, 939, 94, 24)}, 'assets': {(2087, 1013, 62, 14)}, 'ocrv1': {(2087, 1053, 53, 16)}, 'OUTLINE': {(2045, 1087, 86, 45)}, 'TIMELINE': {(2045, 1149, 92, 15)}, 'version2': {(1948, 1181, 122, 29)}, 'oAo': {(2214, 1183, 95, 25)}, 'CRLF': {(2422, 1187, 49, 18)}, 'Python': {(2502, 1187, 68, 23)}, '385': {(2601, 1187, 44, 18)}, '32bit': {(2653, 1187, 59, 18)}, 'A': {(2743, 1183, 25, 25), (470, 1427, 6, 12)}, '1': {(21, 1409, 42, 33)}, 'ie': {(209, 1408, 46, 32)}, 'onl': {(441, 1404, 27, 21)}, 'ben': {(448, 1423, 14, 24)}, 'Lay': {(328, 1445, 29, 2)}, 'fin': {(2817, 1406, 42, 39)}, 'lide': {(16, 1458, 20, 14)}, 'Panel': {(42, 1458, 37, 14)}, 'Undo': {(120, 1458, 36, 14)}, 'Sideshow': {(198, 1458, 69, 14)}, 'Home': {(294, 1460, 39, 12)}, 'Screen': {(339, 1459, 48, 13)}, 'Upload': {(414, 1458, 48, 16)}, 'Cloud': {(487, 1458, 39, 14)}, 'Confirm': {(1252, 1458, 54, 14)}, 'Not': {(1342, 1460, 25, 12)}, 'this': {(1373, 1458, 24, 14)}, 'person': {(1402, 1462, 48, 12)}, 'Add': {(1492, 1458, 27, 14)}, 'Name': {(1525, 1460, 39, 12)}, 'Dont': {(1607, 1458, 36, 14)}, 'show': {(1648, 1458, 35, 14)}, 'again': {(1690, 1462, 38, 13)}, '0': {(20, 1489, 7, 11)}, 'catalog': {(2680, 1481, 46, 31)}, 'au1664797655': {(2731, 1491, 106, 11)}, 'TY': {(1254, 1554, 19, 10)}, 'ENG': {(2379, 1540, 49, 20)}, '2338': {(2720, 1539, 63, 20)}, 'IN': {(2392, 1575, 23, 20)}, 'S': {(2470, 1552, 37, 26)}, 'Q': {(2524, 1551, 37, 30)}, 'ta': {(2578, 1550, 37, 27)}, '03102022': {(2649, 1573, 134, 20)}, 'Ome': {(49, 1530, 836, 81)}, 'Pw': {(514, 1505, 174, 115)}, 'OX': {(744, 1505, 145, 115)}, 'o': {(1276, 1543, 208, 68), (1039, 1546, 148, 65)}}
+# this function will get 2D array each array having vertically aligned text set with in limit function getClusterOfWordsWithInRectangle
+def mergeEachRowOfClusterInSentence(multiple_cluster):
+    contanier={}
+    for row in multiple_cluster:
+        sentence=''
+        x=y=w=h=0 
+        for word_tuple in row:
+            sentence+=str(word_tuple[0])+' '
+            x=(word_tuple[1])
+            y=(word_tuple[2])
+            w=(row[-1][1]-row[0][1])
+            h=(word_tuple[4])
+        contanier[sentence.strip()]=(x,y,w,h)
+    return contanier
 
 
-# getLocation('Events',ocr_extracted_dict)
 
-            
-# print(getLocation('Upload to Cloud',get_unique((get_vertically_aligned_text(getClusterOfMultipleWords('Upload',pixellocationOfword,pixel_matrix))))))
-# import time
-# s=time.time()
-# print(getLocation('Upload to Cloud'))
-# e=time.time()
-# print((e-s))
+
 
 
 
