71f2d3afa41cc3fd7ca9152b1d63fb4b37cba4c8
Pradeep Kumar <kumarp@adobe.com>
2022-11-24 17:47:50 +0530

    working code

--- pywinauto/root.py
+++ pywinauto/root.py
@@ -1,6 +1,6 @@
 from pywinauto import Application
 import pywinauto
-import logging ,time
+import logging 
 import pyautogui as pa
 # pywinauto.windows.uia_element_info.UIAElementInfo.use_raw_view_walker = True
 
@@ -19,9 +19,8 @@ class rootApp:
     def startEOApplication(self):
         try:
             self.app=Application(backend="uia").start(rootApp.EO_EXE_PATH)
-            # self.app=Application(backend="uia").connect(pid=8660, name="Adobe Elements 2023 Organizer")
+            # self.app=Application(backend="uia").connect(pid=16464, name="Adobe Elements 2022 Organizer")
             rootApp.pid=self.app.process
-            time.sleep(20)
             self.main_window=self.app.window(name_re=r".*Organizer")
             self.main_window.set_focus()
             # self.main_window.dump_tree()
